6247bc6743ce6be31acebaf93be986991d5f7dff
Maythee Anegboonlap <null@llun.in.th>
2011-12-20 13:44:01 +0700

    Add mock module to exports

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ exports.FrameworkError = require('./lib/error').FrameworkError;
 
 exports.Crossweb = require('./lib/crossweb').Crossweb;
 exports.Router = require('./lib/router').Router;
+
+exports.Test = {
+  MockRequestResponse: require('./test/MockRequestResponse')
+}
